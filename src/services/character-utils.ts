19c0b75aa0259a078d41abfb363296234ba2586a
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-03-30 20:41:40 -0700

    adjust styles and add loading spinner

--- src/services/character-utils.ts
+++ src/services/character-utils.ts
@@ -1,5 +1,5 @@
 export async function getCharacters() {
-  const res = await fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters?perPage=5')
+  const res = await fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters?perPage=4')
   const characters = await res.json()
 
   return characters
